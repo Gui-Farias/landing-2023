1ae3ea9b169ed8e602fbab6c1159b73f03971036
Guilherme Farias <guilhermefarias@Guilhermes-MacBook-Pro-2.local>
2023-03-21 11:26:46 -0300

    adding form and cv for donwload

--- src/lib/components/Form/Form.svelte
+++ src/lib/components/Form/Form.svelte
@@ -1,13 +1,62 @@
 <script>
   import Input from '$lib/atoms/Input/Input.svelte';
-  
+  import emailjs from '@emailjs/browser';
+
+
   import './style.scss';
+
+  import { onMount } from 'svelte';
+
+ onMount(() => {
+   
+   //   const submitButton = document.getElementById("btn-submit");
+   
+//   submitButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     const name = document.getElementById("Nome:")?.value;
+//     const email = document.getElementById("Email:")?.value;
+//     const mesage = document.getElementById("Mensagem:")?.value;
+
+//     if (!name || !email || !mesage) {
+//       alert('Por favor preencha todo os campos do formulario!')
+//     } else {
+//       console.log('ok');
+//     }
+
+//   });
+})
+
+function sendEmail(e) {
+  emailjs.init('JL8Mpz8IZ3POXOuvS');
+
+  const serviceID = 'default_service';
+  const templateID = 'template_03020sb';
+
+  var templateParams = {
+    name: e.target[0].value,
+    email: e.target[1].value,
+    message: e.target[2].value
+  };
+
+  emailjs.send(serviceID, templateID, templateParams)
+    .then((response) => {
+      console.log("Email enviado com sucesso", response.status, response.text);
+      alert("Sua mensagem foi enviada, obrigado!");
+      document.getElementById('form').reset(); // Clear the form fields
+    }, (error) => {
+      console.error("Error:", error);
+      alert("Oops! Algo deu errado com sua mensagem, por favor atualizar e mandar novamente");
+    });
+  }
+ 
+
 </script>
 
-<form action="" class="form">
+<form action="" class="form" id="form" on:submit|preventDefault={sendEmail}>
   <Input name='Nome:'/>
   <Input name='Email:' type='email'/>
   <Input name='Mensagem:' type='textarea'/>
 
-  <button class="btn btn-primary">Enviar Email</button>
+  <button class="btn btn-primary" id="btn-submit">Enviar Email</button>
 </form> 
--- src/lib/components/btn/btn.svelte
+++ src/lib/components/btn/btn.svelte
@@ -2,6 +2,8 @@
 
   import './style.scss'
 
+  import cv from '$lib/gui-farias-cv.pdf';
+
   export let text = 'My text';
   export let link = '';
 
@@ -9,6 +11,12 @@
 
 {#if link != ''}
 <a href={link} target="_blank" rel="noopener noreferrer">
+  <button class="btn btn-primary">
+    {text}
+  </button>
+</a>
+{:else if text == 'Baixar CV'}
+<a href={cv} target="_blank" rel="noopener noreferrer" download='gui-farias-cv.pdf'>
   <button class="btn btn-primary">
     {text}
   </button>
