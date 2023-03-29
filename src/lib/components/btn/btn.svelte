f95afa2753f0363b0773dfb14fe7688ca778c3c9
Guilherme Farias <guilhermefarias@Guilhermes-MacBook-Pro-2.local>
2023-03-29 12:57:27 -0300

    adding cv in english to download

--- src/lib/components/btn/btn.svelte
+++ src/lib/components/btn/btn.svelte
@@ -4,6 +4,7 @@
   import './style.scss'
 
   import cv from '$lib/gui-farias-cv.pdf';
+  import cvEn from '$lib/cv-guiFarias-english.pdf';
 
   export let text = 'My text';
   export let link = '';
@@ -20,7 +21,12 @@
 <a href={cv} target="_blank" rel="noopener noreferrer" download='gui-farias-cv.pdf' aria-label="{text}">
   <button class="btn btn-primary">
     {text}
-    
+  </button>
+</a>
+{:else if text == 'Download CV'}
+<a href={cvEn} target="_blank" rel="noopener noreferrer" download='cv-guiFarias-english.pdf' aria-label="{text}">
+  <button class="btn btn-primary">
+    {text}
   </button>
 </a>
 {:else}
